128cd66f1a190a22a71a7f33d9b5033627d34ea6
Andrew Staroscik <AndrewStaroscik@users.noreply.github.com>
2017-05-01 16:58:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // takes an array and returns the same array shuffled. 
 
-module.exports = funciton(array) {
+module.exports = function(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
     var temp = array[i];
